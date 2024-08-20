7ca103baeef94e5393eb3a4279be6badf5a21f37
Ahmed Ali <ahmedalielian20@gmail.com>
2024-08-20 12:44:59 +0300

    intial

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const middlewares = jsonServer.defaults();
 const port =  3000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/*":"/$1",
+    })
+)
 server.use(router);
 
 server.listen(port);
+
+module.exports=server
