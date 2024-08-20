32b64378727d0262a34e9a848fdb25666104948f
Ahmed Ali <ahmedalielian20@gmail.com>
2024-08-20 13:39:26 +0300

    ss

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port =  3000; //  chose port from here like 8080, 3001
+const port =  8000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(
