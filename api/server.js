dd9bd0988b4ecb2fd5c14828de614a40e85caff6
Ahmed Ali <ahmedalielian20@gmail.com>
2024-08-24 23:42:08 +0300

    add cor option

--- api/server.js
+++ api/server.js
@@ -1,33 +1,44 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server');
 const cors = require('cors');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
+// const fs = require('fs');
+// const path = require('path');
+// const filePath = path.join('db.json');
 // const data = fs.readFileSync(filePath, "utf-8");
 // const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+// const router = jsonServer.router(db);
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json');
 
-const middlewares = jsonServer.defaults()
-server.use(cors());
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+// Customize CORS options
+const corsOptions = {
+    origin: '*', // Allow all origins. Change this to your specific domain if needed.
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    exposedHeaders: ['Content-Length', 'X-Total-Count'], // Expose headers
+};
+
+server.use(cors(corsOptions)); // Apply the CORS middleware with options
+
+server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+    '/blog/:resource/:id/show': '/:resource/:id',
+}));
+
+server.use(router);
+
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
